f99d3dab5e67848f570b94204fe092d4666c7c9f
Tian Feng <feng.txy@outlook.com>
2019-06-28 20:10:25 -0700

    Update 973. K Closest Points to Origin.py

--- 973. K Closest Points to Origin.py
+++ 973. K Closest Points to Origin.py
@@ -5,12 +5,9 @@ class Solution(object):
         :type K: int
         :rtype: List[List[int]]
         """
-        points.sort(key = lambda point: self.fromOrigin(point))
+        points.sort(key = lambda p:p[0]**2+p[1]**2)
         return points[:K]
-
-
-    def fromOrigin(self, point):
-        return (point[0]**2 + point[1]**2)**0.5
+    
 # class Solution(object):
 #     def kClosest(self, points, K):
 #         """
