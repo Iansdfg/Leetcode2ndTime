2522097d6f7894829fb023b93c4112fcb79e6faf
Tian Feng <feng.txy@outlook.com>
2019-07-01 13:32:45 -0700

    Update 238. Product of Array Except Self.py

--- 238. Product of Array Except Self.py
+++ 238. Product of Array Except Self.py
@@ -1,3 +1,24 @@
+# class Solution(object):
+#     def productExceptSelf(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: List[int]
+#         """
+#         lr = [0]*len(nums)
+#         tmp = 1
+#         for i in range(len(nums)):
+#             lr[i] = tmp
+#             tmp *= nums[i]
+#         tmp = 1
+#         rl = [0]*len(nums)
+#         for i in range(len(nums)-1,-1,-1):
+#             rl[i] = tmp
+#             tmp *= nums[i]
+#         res = []
+#         for i in range(len(nums)):
+#             res.append(lr[i]*rl[i])
+#         return res
+                 
 class Solution(object):
     def productExceptSelf(self, nums):
         """
