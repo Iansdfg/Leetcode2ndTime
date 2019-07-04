d74292def3b4c2d19901b00e538142557e2e9c81
Tian Feng <feng.txy@outlook.com>
2019-07-03 22:31:25 -0700

    Create 1. Two Sum.py

---
+++ 1. Two Sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dic = {}
+        for pos, val in enumerate(nums):
+            if val not in dic:
+                dic[target-val] = pos
+            else:
+                return [dic[val], pos]
+        
