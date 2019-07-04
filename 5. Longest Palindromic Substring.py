3c43ee59651e1cd40827261769f5a17efbba1542
Tian Feng <feng.txy@outlook.com>
2019-07-03 22:33:43 -0700

    Update 5. Longest Palindromic Substring.py

--- 5. Longest Palindromic Substring.py
+++ 5. Longest Palindromic Substring.py
@@ -13,12 +13,10 @@ class Solution(object):
             if len(odd)>len(res):
                  res = odd
         return res
-        
-        
-        
+       
     def palindrome(self, s, l, r):
         while l>=0 and r<len(s) and s[l] == s[r]:
             l-=1
             r+=1
         return s[l+1: r]
-        
+  
