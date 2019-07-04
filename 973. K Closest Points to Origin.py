02e9ac839d9623c657b2d28832d4d4c753416ae9
Tian Feng <feng.txy@outlook.com>
2019-07-03 22:34:41 -0700

    Update 973. K Closest Points to Origin.py

--- 973. K Closest Points to Origin.py
+++ 973. K Closest Points to Origin.py
@@ -1,13 +1,3 @@
-class Solution(object):
-    def kClosest(self, points, K):
-        """
-        :type points: List[List[int]]
-        :type K: int
-        :rtype: List[List[int]]
-        """
-        points.sort(key = lambda p:p[0]**2+p[1]**2)
-        return points[:K]
-    
 # class Solution(object):
 #     def kClosest(self, points, K):
 #         """
@@ -15,11 +5,20 @@ class Solution(object):
 #         :type K: int
 #         :rtype: List[List[int]]
 #         """
-#         dis = []
-#         for p in points:
-#             d = math.sqrt(p[0] ** 2 + p[1] ** 2)
-#             dis.append((d, p))
-#         heapq.heapify(dis)
-#         # print(dis)
-#         return [d[1] for d in heapq.nsmallest(K, dis)]
+#         points.sort(key = lambda p:p[0]**2+p[1]**2)
+#         return points[:K]    
+class Solution(object):
+    def kClosest(self, points, K):
+        """
+        :type points: List[List[int]]
+        :type K: int
+        :rtype: List[List[int]]
+        """
+        dis = []
+        for p in points:
+            d = math.sqrt(p[0] ** 2 + p[1] ** 2)
+            dis.append((d, p))
+        heapq.heapify(dis)
+        # print(dis)
+        return [d[1] for d in heapq.nsmallest(K, dis)]
         
