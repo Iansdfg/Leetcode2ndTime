ecfe11ebe1e53477bff5aebe09b4020a7b0d3a70
Tian Feng <feng.txy@outlook.com>
2019-07-05 23:21:55 -0700

    Create 139. Word Break.py

---
+++ 139. Word Break.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        dp = [1]+[0]*len(s)
+        for i in xrange(1,len(dp)):
+            for j in xrange(i):
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = 1
+        return dp[-1]
