f4533daad746a59005b5a8083483aa1cfe9f698d
Tian Feng <feng.txy@outlook.com>
2019-07-06 09:00:20 -0700

    Update 139. Word Break.py

--- 139. Word Break.py
+++ 139. Word Break.py
@@ -6,8 +6,8 @@ class Solution(object):
         :rtype: bool
         """
         dp = [1]+[0]*len(s)
-        for i in xrange(1,len(dp)):
-            for j in xrange(i):
+        for i in range(1,len(dp)):
+            for j in range(i):
                 if dp[j] and s[j:i] in wordDict:
                     dp[i] = 1
         return dp[-1]
