9e7f8ea24dcf56211d89e048c221a38794138745
Tian Feng <feng.txy@outlook.com>
2019-07-06 13:41:09 -0700

    Update 136. Single Number.py

--- 136. Single Number.py
+++ 136. Single Number.py
@@ -5,17 +5,17 @@ class Solution(object):
         :rtype: int
         """
         return sum(set(nums))*2-sum(nums)
-class Solution(object):
-    def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        res = []
-        for num in nums:
-            if num in res:
-                res.remove(num)
-            else:
-                res.append(num)
-        return res[0]
+# class Solution(object):
+#     def singleNumber(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         res = []
+#         for num in nums:
+#             if num in res:
+#                 res.remove(num)
+#             else:
+#                 res.append(num)
+#         return res[0]
         
