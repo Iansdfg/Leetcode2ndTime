821a9d8bb3624cd1ef0565920efd9686b34fc4ad
Tian Feng <feng.txy@outlook.com>
2019-07-07 14:39:07 -0700

    Update 17. Letter Combinations of a Phone Number.py

--- 17. Letter Combinations of a Phone Number.py
+++ 17. Letter Combinations of a Phone Number.py
@@ -1,9 +1,40 @@
+# class Solution(object):
+#     def letterCombinations(self, digits):
+#         """
+#         :type digits: str
+#         :rtype: List[str]
+#         """
+#         dic = {'0': '',
+#                '1': '*',
+#                '2': 'abc',
+#                '3': 'def',
+#                '4': 'ghi',
+#                '5': 'jkl',
+#                '6': 'mno',
+#                '7': 'pqrs',
+#                '8': 'tuv',
+#                '9': 'wxyz',
+#         }
+#         results = []
+#         for digit in digits:
+#             if results == []:
+#                 for char in dic[digit]:
+#                     results.append(char)
+#             else:
+#                 temp = []
+#                 for result in results:
+#                     for ele in dic[digit]:
+#                         temp.append(result+ele)
+#                 results = temp
+#         return results
+            
 class Solution(object):
     def letterCombinations(self, digits):
         """
         :type digits: str
         :rtype: List[str]
         """
+        res = []
         dic = {'0': '',
                '1': '*',
                '2': 'abc',
@@ -15,20 +46,12 @@ class Solution(object):
                '8': 'tuv',
                '9': 'wxyz',
         }
-        results = []
-        for digit in digits:
-            if results == []:
-                for char in dic[digit]:
-                    results.append(char)
+        def dfs(path, digits):
+            if not digits:
+                res.append(path)
             else:
-                temp = []
-                for result in results:
-                    for ele in dic[digit]:
-                        temp.append(result+ele)
-                results = temp
-        return results
-            
-
-
-        
-        
+                for letter in dic[digits[0]]:
+                    dfs(path+letter, digits[1:])
+        if digits:
+            dfs('', digits)
+        return res
