d0aa824db14daa50d31722c48eba1fb1b8d3a097
Tian Feng <feng.txy@outlook.com>
2019-07-09 19:42:19 -0700

    Create 29. Divide Two Integers.py

---
+++ 29. Divide Two Integers.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        sign = 1 if dividend*divisor>0 else -1
+        dividend = abs(dividend); divisor = abs(divisor)
+        if sign == 1 and dividend < divisor:return 0
+        summ,count,res = 0,0,0
+        while dividend >= divisor:
+            summ = divisor
+            count = 1
+            while summ + summ <= dividend:
+                summ += summ
+                count += count
+            dividend -= summ
+            res += count
+        if sign*res>=2147483647:
+            return 2147483647
+        elif sign*res<=-2147483648:
+            return -2147483648
+        else:
+            return sign*res
+        
