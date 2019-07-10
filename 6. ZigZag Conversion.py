9d3137802571da73ffc6b2ef43b6f2e8e2dbdfb1
Tian Feng <feng.txy@outlook.com>
2019-07-10 10:37:41 -0700

    Update 6. ZigZag Conversion.py

--- 6. ZigZag Conversion.py
+++ 6. ZigZag Conversion.py
@@ -5,22 +5,22 @@ class Solution(object):
         :type numRows: int
         :rtype: str
         """
-        if numRows == 1:return s
         cycle = 2*numRows-2
+        if numRows==1:return s
+        row_num, cyc_num = 0,0
         res = ''
-        cycle_num, row_num = 0, 0
         while len(res)<len(s):
-            if cycle*cycle_num+row_num>len(s)-1:
-                cycle_num = 0
-                row_num+=1
-            zig = cycle*cycle_num+row_num
-            zag = cycle*(cycle_num+1)-row_num
-            if row_num == 0 or row_num == numRows-1: 
+            if cyc_num*cycle+row_num>len(s)-1:
+                cyc_num = 0
+                row_num += 1
+            zig = cyc_num*cycle+row_num
+            zag = (cyc_num+1)*cycle-row_num
+            if row_num == 0 or row_num == numRows-1:
                 res+=s[zig]
             else:
-                if zag > len(s)-1:
+                if zag>len(s)-1:
                     res+=s[zig]
                 else:
                     res+=s[zig]+s[zag]
-            cycle_num+=1
+            cyc_num+=1
         return res
