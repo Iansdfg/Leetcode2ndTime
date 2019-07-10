e35c2e640f65523a5fffbf2c5f92e99a8ab136d7
Tian Feng <feng.txy@outlook.com>
2019-07-10 11:03:26 -0700

    Update 29. Divide Two Integers.py

--- 29. Divide Two Integers.py
+++ 29. Divide Two Integers.py
@@ -5,22 +5,21 @@ class Solution(object):
         :type divisor: int
         :rtype: int
         """
-        sign = 1 if dividend*divisor>0 else -1
-        dividend = abs(dividend); divisor = abs(divisor)
-        if sign == 1 and dividend < divisor:return 0
+        sign = 1 if (dividend>0 and divisor>0) or (dividend<0 and divisor<0) else -1
+        dividend, divisor = abs(dividend), abs(divisor)
+        if sign == 1 and divisor>dividend:return 0
         summ,res = 0,0
-        while dividend>=divisor:
+        while divisor<=dividend:
             summ = divisor
             count = 1
-            while summ+summ<=dividend:
+            while summ + summ<=dividend:
                 summ+=summ
                 count+=count
-            dividend-=summ
+            dividend -= summ
             res+=count
-        if sign*res>=2147483647:
-            return 2147483647
-        elif sign*res<=-2147483648:
-            return -2147483648
-        else:
+        if -2**31<sign*res<2**31-1:
             return sign*res
-        
+        elif -2**31>=sign*res:
+            return -2**31
+        elif sign*res>=2**31-1:
+            return 2**31-1
