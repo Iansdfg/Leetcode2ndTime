5d7a9f668161336629d5e2d957b28f2754043e03
Tian Feng <feng.txy@outlook.com>
2019-07-10 22:02:01 -0700

    Update and rename 31. Next Permutation to 31. Next Permutation.py

--- 31. Next Permutation.py
+++ 31. Next Permutation.py
@@ -7,9 +7,13 @@ class Solution(object):
         for i in range(len(nums)-2, -1, -1):
             if nums[i]<nums[i+1]:
                 for j in range(len(nums)-1, i-1, -1):
-                    if nums[j]>nums[i]:break
-                nums[j], nums[i] = nums[i], nums[j]
-                nums[i+1:] = reversed(nums[i+1:])
-                return
-        nums[:] = reversed(nums)
+                    if nums[j]>nums[i]:
+                        nums[j], nums[i] = nums[i], nums[j]
+                        nums[i+1:] = reversed(nums[i+1:])
+                        return 
+        nums[:] = reversed(nums[:])
         return 
+
+            
+            
+        
