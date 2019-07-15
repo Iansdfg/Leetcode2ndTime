fecb91d1f198cdc7d453b30d431ec6f183bc4a2f
Tian Feng <feng.txy@outlook.com>
2019-07-15 10:57:55 -0700

    Update 36. Valid Sudoku.py

--- 36. Valid Sudoku.py
+++ 36. Valid Sudoku.py
@@ -4,34 +4,34 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
-        return self.validRow(board) and self.validCol(board) and self.validSqr(board)
+        return self.isValidRow(board) and self.isValidCol(board) and self.isValidSqr(board)
         
-    def validRow(self, board):
-        for row in range(len(board)):
-            thisRow = [x for x in board[row] if x != '.']
-            if len(thisRow) != len(set(thisRow)):
+    def isValidRow(self, board):
+        for r in range(len(board)):
+            row = [ ele for ele in board[r] if ele != '.' ]
+            if len(row) != len(set(row)):
                 return False
         return True
-    
-    def validCol(self, board):
-        for col in range(len(board)):
-            thisCol = [board[r][col] for r in range(len(board)) if board[r][col] != '.']
-            if len(set(thisCol)) != len(thisCol):
+            
+        
+    def isValidCol(self, board):
+        for c in range(len(board)):
+            col = [board[r][c] for r in range(len(board)) if board[r][c] != '.']
+            if len(col) != len(set(col)):
                 return False
         return True
-    
-    def validSqr(self, board):
-        for row in range(0, len(board),3):
-            for col in range(0, len(board),3):
-                thisSqr = []
+        
+        
+    def isValidSqr(self, board):
+        for row in range(0,len(board),3):
+            for col in range(0,len(board),3):
+                sqr = []
                 for i in range(3):
                     for j in range(3):
-                        if board[row+i][col+j]!='.':
-                            thisSqr.append(board[row+i][col+j])
-                if len(set(thisSqr)) != len(thisSqr):
-                    return False
+                        ele = board[row+i][col+j]
+                        if ele!='.':
+                            sqr.append(ele)
+                        if len(sqr) != len(set(sqr)):
+                            return False            
         return True
-
         
-        
-    
