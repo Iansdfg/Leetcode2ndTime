21a2ea186801b983c151acb53f5aa13cc5fff27f
Tian Feng <feng.txy@outlook.com>
2019-07-15 13:30:41 -0700

    Update 39. Combination Sum.py

--- 39. Combination Sum.py
+++ 39. Combination Sum.py
@@ -7,16 +7,16 @@ class Solution(object):
         """
         res = []
         candidates.sort()
-        self.dfs(candidates, target, 0, res, [])
+        self.findSum(candidates, target, 0, [], res)
         return res
     
-    def dfs(self, candidates, target, depth, res, path):
-        if target<0:
+    def findSum(self, candidates, target, depth, path, res):
+        if target < 0:
             return 
-        if target==0:
+        if target == 0:
             res.append(path)
-            return 
         for i in range(depth, len(candidates)):
+            curr = candidates[i]
             if candidates[depth]>target:
                 return 
-            self.dfs(candidates, target-candidates[i], i, res, path+[candidates[i]])
+            self.findSum(candidates, target-curr, i, path+[curr], res)
