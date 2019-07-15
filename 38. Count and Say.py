fe10706eba776421ce58046e57472b7beefbd92e
Tian Feng <feng.txy@outlook.com>
2019-07-15 15:21:00 -0700

    Update 38. Count and Say.py

--- 38. Count and Say.py
+++ 38. Count and Say.py
@@ -6,14 +6,13 @@ class Solution(object):
         """
         res = '1'
         for _ in range(n-1):
-            temp, pre, cnt = '', res[0], 0
+            pre, count,temp = res[0], 0,''
             for pos in range(len(res)):
                 if res[pos] == pre:
-                    cnt +=1
+                    count += 1
                 else:
-                    temp+= str(cnt)+pre
-                    cnt = 1
+                    temp += str(count)+pre
+                    count = 1 
                     pre = res[pos]
-            res = temp+str(cnt)+pre
+            res = temp+ str(count)+res[pos]
         return res
-    
