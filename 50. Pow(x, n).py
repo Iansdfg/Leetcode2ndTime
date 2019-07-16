837eeb289b7392aa44049d8de26262090b643a2b
Tian Feng <feng.txy@outlook.com>
2019-07-16 12:18:47 -0700

    Create 50. Pow(x, n).py

---
+++ 50. Pow(x, n).py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if n == 0: return 1
+        if n < 0:
+            x = 1 / x
+            n = -n
+        ans = 1
+        while n:
+            if n % 2:
+                ans *= x
+            x*=x
+            n//=2
+            print(x, n)
+        return ans
