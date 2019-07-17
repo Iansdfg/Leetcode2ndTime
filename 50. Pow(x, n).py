35c15dda4046ea4c1e41cee2c5dbf7930761488c
Tian Feng <feng.txy@outlook.com>
2019-07-17 10:10:09 -0700

    Update 50. Pow(x, n).py

--- 50. Pow(x, n).py
+++ 50. Pow(x, n).py
@@ -5,10 +5,13 @@ class Solution(object):
         :type n: int
         :rtype: float
         """
+        if n == 0: return 1
         sign = 1 if n>0 else -1
-        n, ans = abs(n), 1
+        res, n = 1, abs(n)
         while n:
-            if n%2: ans*=x
-            n//=2
+            if n%2:res*=x
             x*=x
-        return ans if sign>0 else 1/ans
+            n//=2
+
+        return res if sign>0 else 1/res
+        
