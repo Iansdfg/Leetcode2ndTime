a70e2dda489b8919791d3087259b464468cb203c
Tian Feng <feng.txy@outlook.com>
2019-07-18 13:47:21 -0700

    Update 53. Maximum Subarray.py

--- 53. Maximum Subarray.py
+++ 53. Maximum Subarray.py
@@ -1,40 +1,10 @@
-# class Solution(object):
-#     def maxSubArray(self, nums):
-#         """
-#         :type nums: List[int]
-#         :rtype: int
-#         """
-#         res, summ = float('-inf'), 0
-#         for num in nums:
-#             summ = max(num, num+summ)
-#             res = max(res, summ)
-#         return res
-        
 class Solution(object):
     def maxSubArray(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        return self.helper(nums, 0, len(nums)-1)
-     
-    def cross_sum(self, nums, left, right, p): 
-            if left == right:
-                return nums[left]
-
-            left_subsum = float('-inf')
-            curr_sum = 0
-            for i in range(p, left - 1, -1):
-                curr_sum += nums[i]
-                left_subsum = max(left_subsum, curr_sum)
-
-            right_subsum = float('-inf')
-            curr_sum = 0
-            for i in range(p + 1, right + 1):
-                curr_sum += nums[i]
-                right_subsum = max(right_subsum, curr_sum)
-
-            return left_subsum + right_subsum   
+        return self.helper(nums, 0, len(nums)-1) 
     
     def helper(self, nums, left, right): 
         if left == right:
@@ -47,3 +17,21 @@ class Solution(object):
         cross_sum = self.cross_sum(nums, left, right, p)
         
         return max(left_sum, right_sum, cross_sum)
+    
+    def cross_sum(self, nums, left, right, p): 
+        if left == right:
+            return nums[left]
+
+        left_subsum = float('-inf')
+        curr_sum = 0
+        for i in range(p, left - 1, -1):
+            curr_sum += nums[i]
+            left_subsum = max(left_subsum, curr_sum)
+
+        right_subsum = float('-inf')
+        curr_sum = 0
+        for i in range(p + 1, right + 1):
+            curr_sum += nums[i]
+            right_subsum = max(right_subsum, curr_sum)
+
+        return left_subsum + right_subsum  
