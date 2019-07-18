5dd30734806994b65f033460968031025e2a601e
Tian Feng <feng.txy@outlook.com>
2019-07-18 15:34:59 -0700

    Update 53. Maximum Subarray.py

--- 53. Maximum Subarray.py
+++ 53. Maximum Subarray.py
@@ -4,34 +4,32 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        return self.helper(nums, 0, len(nums)-1) 
-    
-    def helper(self, nums, left, right): 
-        if left == right:
+        return self.helper(nums, 0, len(nums)-1)
+        
+    def helper(self, nums, left, right):
+        if left==right:
             return nums[left]
-        
-        p = (left + right) // 2
-            
-        left_sum = self.helper(nums, left, p)
-        right_sum = self.helper(nums, p + 1, right)
-        cross_sum = self.cross_sum(nums, left, right, p)
-        
+        mid = (left+right)//2
+        left_sum = self.helper(nums, left, mid)
+        right_sum = self.helper(nums, mid+1, right)
+        cross_sum = self.cross(nums, left, right, mid)
         return max(left_sum, right_sum, cross_sum)
-    
-    def cross_sum(self, nums, left, right, p): 
+        
+    def cross(self, nums, left, right, mid):
         if left == right:
             return nums[left]
-
-        left_subsum = float('-inf')
-        curr_sum = 0
-        for i in range(p, left - 1, -1):
-            curr_sum += nums[i]
-            left_subsum = max(left_subsum, curr_sum)
-
-        right_subsum = float('-inf')
-        curr_sum = 0
-        for i in range(p + 1, right + 1):
-            curr_sum += nums[i]
-            right_subsum = max(right_subsum, curr_sum)
-
-        return left_subsum + right_subsum  
+        
+        right_sum= 0
+        right_curr= float("-inf")
+        for i in range(mid+1, right+1):
+            right_sum += nums[i]
+            right_curr = max(right_sum, right_curr)
+            
+        left_sum= 0
+        left_curr= float("-inf")
+        for i in range(mid, left-1, -1):
+            left_sum += nums[i]
+            left_curr = max(left_sum, left_curr)
+        
+        return left_curr+right_curr
+            
