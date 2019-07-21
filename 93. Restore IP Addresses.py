7c080089e2417bc06f25e6b5adff5ab08ea36fbb
Tian Feng <feng.txy@outlook.com>
2019-07-21 12:54:16 -0700

    Update 93. Restore IP Addresses.py

--- 93. Restore IP Addresses.py
+++ 93. Restore IP Addresses.py
@@ -11,12 +11,14 @@ class Solution(object):
         return res
         
     def dfs(self, s, path, res):
+        if len(s) > (4 - len(path)) * 3:
+            return
         if not s and len(path) == 4:
             res.append('.'.join(path))
             return
-        for i in range(1, 4):
+        for i in range(1,4):
             if i > len(s):
                 continue
-            number = int(s[:i])
-            if str(number) == s[:i] and number <= 255:
-                self.dfs(s[i:], path + [s[:i]], res)
+            num = int(s[:i])
+            if str(num) == s[:i] and num<=255:
+                self.dfs(s[i:], path+[s[:i]], res)
