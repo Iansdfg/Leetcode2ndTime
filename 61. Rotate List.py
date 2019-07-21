55955a028d1a335b7f810782c3bad3a2a70baca1
Tian Feng <feng.txy@outlook.com>
2019-07-20 21:47:21 -0700

    Create 61. Rotate List.py

---
+++ 61. Rotate List.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def rotateRight(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        if not head: return head
+        curr = fast = slow = head
+        count = 0
+        while curr:
+            count+=1
+            curr = curr.next
+        k = k%count
+        while k:
+            if not fast:
+                fast = head
+            fast = fast.next
+            count+=1
+            k-=1
+        if not fast:
+            return head
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        fast.next = head
+        newHead = slow.next
+        slow.next = None
+        return newHead
+        
