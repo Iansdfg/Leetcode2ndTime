0bb210b2c3c9b186e4ea6e799e40822e6703a04e
Tian Feng <feng.txy@outlook.com>
2019-07-21 12:33:52 -0700

    Create 93. Restore IP Addresses.py

---
+++ 93. Restore IP Addresses.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def restoreIpAddresses(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        if len(s) > 12:
+            return []
+        res = []
+        self.dfs(s, [], res)
+        return res
+        
+    def dfs(self, s, path, res):
+        if not s and len(path) == 4:
+            res.append('.'.join(path))
+            return
+        for i in range(1, 4):
+            if i > len(s):
+                continue
+            number = int(s[:i])
+            if str(number) == s[:i] and number <= 255:
+                self.dfs(s[i:], path + [s[:i]], res)
