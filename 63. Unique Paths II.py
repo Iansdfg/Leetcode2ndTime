fdb6adb2b6ede91a46d7c402edaaa4a61c0b67fe
Tian Feng <feng.txy@outlook.com>
2019-07-21 11:43:50 -0700

    Update 63. Unique Paths II.py

--- 63. Unique Paths II.py
+++ 63. Unique Paths II.py
@@ -8,23 +8,15 @@ class Solution(object):
             return 0
 
         m, n = len(obstacleGrid), len(obstacleGrid[0])
-
-        dp = [[0 for i in range(n+1)]for i in range(m+1)]
-
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                if obstacleGrid[i - 1][j - 1] == 1:
+        dp = [[0 for _ in xrange(n+1)]for _ in xrange(m+1)]
+        for i in xrange(1,m+1):
+            for j in xrange(1,n+1):
+                if obstacleGrid[i-1][j-1] == 1:
                     dp[i][j] = 0
                 else:
-                    if i == j == 1:
+                    if i == 1 and j == 1:
                         dp[i][j] = 1
                     else:
-                        dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
-        print(dp)
+                        dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        # print(dp)
         return dp[m][n]
-
-
-
-
-
-
