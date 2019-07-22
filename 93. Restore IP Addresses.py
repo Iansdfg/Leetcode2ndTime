06cd4935786f8ed8249528e333ae28de4bc3adba
Tian Feng <feng.txy@outlook.com>
2019-07-21 18:37:57 -0700

    Update 93. Restore IP Addresses.py

--- 93. Restore IP Addresses.py
+++ 93. Restore IP Addresses.py
@@ -4,21 +4,22 @@ class Solution(object):
         :type s: str
         :rtype: List[str]
         """
-        if len(s) > 12:
+        if len(s)>12:
             return []
         res = []
         self.dfs(s, [], res)
         return res
-        
+    
     def dfs(self, s, path, res):
-        if len(s) > (4 - len(path)) * 3:
-            return
-        if not s and len(path) == 4:
-            res.append('.'.join(path))
-            return
+        if len(s)<=0 and len(path)==4:
+            res.append(".".join(path))
+            return 
         for i in range(1,4):
-            if i > len(s):
-                continue
+            # 能否满足在最多4个3位数字组成
+            if len(s)//3 + len(path) > 4:
+                return
+            if i>len(s): continue
             num = int(s[:i])
-            if str(num) == s[:i] and num<=255:
+            if num<=255 and str(num) == s[:i]:
                 self.dfs(s[i:], path+[s[:i]], res)
+        
