bbc5c9d9fd91256ea0e99817413d403a8f64e4ba
Tian Feng <feng.txy@outlook.com>
2019-07-21 20:39:43 -0700

    Create 67. Add Binary.py

---
+++ 67. Add Binary.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        if not a or not b:
+            return a or b
+        if a[-1] == '1' and b[-1] == '1':
+            return self.addBinary(self.addBinary(a[:-1], b[:-1]), '1') + '0'
+        elif a[-1] == '0' and b[-1] == '0':
+            return self.addBinary(a[:-1], b[:-1]) + '0'
+        else:
+            return self.addBinary(a[:-1], b[:-1]) + '1'
+
+        
