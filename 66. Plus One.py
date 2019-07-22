76a1686ff191f99fb936e0abd6424feb4a9b1fd1
Tian Feng <feng.txy@outlook.com>
2019-07-21 19:29:54 -0700

    Update 66. Plus One.py

--- 66. Plus One.py
+++ 66. Plus One.py
@@ -12,10 +12,8 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        addOne = 1
-        for i in range(len(digits)-1, -1, -1):
-            if addOne:
-                digits[i]+=1
+        for i in xrange(len(digits)-1, -1, -1):
+            digits[i]+=1
             if digits[i]<10:
                 return digits
             else:
