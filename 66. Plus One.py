74cc718ae446f64d051c8c30b02803d171eb43ea
Tian Feng <feng.txy@outlook.com>
2019-07-21 19:29:11 -0700

    Update 66. Plus One.py

--- 66. Plus One.py
+++ 66. Plus One.py
@@ -1,8 +1,25 @@
+# class Solution(object):
+#     def plusOne(self, digits):
+#         """
+#         :type digits: List[int]
+#         :rtype: List[int]
+#         """
+
+#         return str(int(''.join([str(digit) for digit in digits ]))+1)
 class Solution(object):
     def plusOne(self, digits):
         """
         :type digits: List[int]
         :rtype: List[int]
         """
-
-        return str(int(''.join([str(digit) for digit in digits ]))+1)
+        addOne = 1
+        for i in range(len(digits)-1, -1, -1):
+            if addOne:
+                digits[i]+=1
+            if digits[i]<10:
+                return digits
+            else:
+                digits[i] = 0
+        return [1]+ digits
+                
+                
