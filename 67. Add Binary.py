edb6bd36249ca4f28d0fd82b875e16f062859413
Tian Feng <feng.txy@outlook.com>
2019-07-22 23:03:39 -0700

    Update 67. Add Binary.py

--- 67. Add Binary.py
+++ 67. Add Binary.py
@@ -7,11 +7,9 @@ class Solution(object):
         """
         if not a or not b:
             return a or b
-        if a[-1] == '1' and b[-1] == '1':
-            return self.addBinary(self.addBinary(a[:-1], b[:-1]), '1') + '0'
-        elif a[-1] == '0' and b[-1] == '0':
-            return self.addBinary(a[:-1], b[:-1]) + '0'
+        if a[-1] == '0' and b[-1] == '0':
+            return self.addBinary(a[:-1], b[:-1])+'0'
+        elif a[-1] != b[-1] :
+            return self.addBinary(a[:-1], b[:-1])+'1'
         else:
-            return self.addBinary(a[:-1], b[:-1]) + '1'
-
-        
+            return self.addBinary(self.addBinary(a[:-1], b[:-1]), '1')+'0'
