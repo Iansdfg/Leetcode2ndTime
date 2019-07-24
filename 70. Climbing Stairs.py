61a6775ef878cc8301c6bdbc95c3a771a8b9ef6f
Tian Feng <feng.txy@outlook.com>
2019-07-23 19:52:22 -0700

    Update 70. Climbing Stairs.py

--- 70. Climbing Stairs.py
+++ 70. Climbing Stairs.py
@@ -4,8 +4,8 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        dp = [1 for _ in range(n+1)]
-        for i in range(2,n+1):
-            dp[i]=dp[i-1]+dp[i-2]
+        dp = [1,1]+[0]*(n-1)
+        for i in xrange(2,n+1):
+            dp[i] = dp[i-1]+dp[i-2]
         return dp[-1]
-        
+            
