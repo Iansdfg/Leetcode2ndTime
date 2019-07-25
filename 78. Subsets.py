5ae18ef2377390eafd0d4a1bc60343ee5d540581
Tian Feng <feng.txy@outlook.com>
2019-07-25 00:14:07 -0700

    Create 78. Subsets.py

---
+++ 78. Subsets.py
@@ -0,0 +1,17 @@
+
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        self.sub(nums, 0, [], res)
+        return res
+
+    def sub(self, nums, depth, path, res):
+        res.append(path[:])
+        for i in range(depth, len(nums)):
+            self.sub(nums, i + 1, path+[nums[i]], res)
+
+
