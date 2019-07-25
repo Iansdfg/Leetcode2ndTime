2a439dae9ec0bdbf85813de38ae5e7c47e7d281e
Tian Feng <feng.txy@outlook.com>
2019-07-24 22:24:43 -0700

    Update 74. Search a 2D Matrix.py

--- 74. Search a 2D Matrix.py
+++ 74. Search a 2D Matrix.py
@@ -6,19 +6,19 @@ class Solution(object):
         :rtype: bool
         """
         if not matrix or not matrix[0]:return False
-        l,r = 0, len(matrix)
+        l,r = 0, len(matrix)-1
         while l<=r:
             m = (r+l)//2
-            if m >= len(matrix):return False
-            print(m)
-            if target in matrix[m]:
-                return True
-            elif target<matrix[m][0]:
+            if target<matrix[m][0]:
                 r = m-1
             elif target>matrix[m][-1]:
                 l = m+1
-            elif matrix[m][0]<target<matrix[m][-1]:
-                return False
+            else:
+                if target in matrix[m]:
+                    return True
+                else:
+                    return False
+        return False
             
             
         
