70f035ec0cca5e505dca137f2c9c6fa295fa0e5f
Tian Feng <feng.txy@outlook.com>
2019-07-25 17:23:32 -0700

    Update 26. Remove Duplicates from Sorted Array.py

--- 26. Remove Duplicates from Sorted Array.py
+++ 26. Remove Duplicates from Sorted Array.py
@@ -4,11 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums) == 0:
-            return 0
-        count = 0
-        for i in range(len(nums)):
-            if nums[count]!=nums[i]:
-                nums[count+1],nums[i] = nums[i],nums[count+1]
-                count+=1
-        return count+1
+        slow, fast = 0, 1
+        while fast< len(nums):
+            if nums[slow] == nums[fast]:
+                fast+=1
+            else:
+                slow+=1
+                nums[slow] = nums[fast]
+        return slow+1
+                
+        
