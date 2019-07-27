0c9919495c1712be2c295efd053c279cefeef622
Tian Feng <feng.txy@outlook.com>
2019-07-26 23:39:12 -0700

    Create 80. Remove Duplicates from Sorted Array II.py

---
+++ 80. Remove Duplicates from Sorted Array II.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums)<3: return len(nums)
+        slow, fast = 1,2
+        while fast<len(nums):
+            if nums[fast] == nums[slow] == nums[slow-1]:
+                fast += 1
+            else:
+                slow += 1
+                nums[slow] = nums[fast]
+                fast += 1       
+        return slow+1
