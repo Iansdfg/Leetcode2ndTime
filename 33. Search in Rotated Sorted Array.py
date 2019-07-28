fe3c9d73a0b0410fa0e651b594920502acc1dd1d
Tian Feng <feng.txy@outlook.com>
2019-07-27 21:01:34 -0700

    Update 33. Search in Rotated Sorted Array.py

--- 33. Search in Rotated Sorted Array.py
+++ 33. Search in Rotated Sorted Array.py
@@ -5,21 +5,19 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        l, r = 0, len(nums)-1
+        l,r = 0, len(nums)-1
         while l<=r:
-            m = (r+l)//2
-            if nums[m] == target:return m
-            if nums[m]>=nums[l]:
-                if nums[l]<= target <= nums[m]:
+            m = (l+r)//2
+            if target == nums[m]:return m
+            elif nums[m]>=nums[l]:
+                if  nums[l]<=target<nums[m]:
                     r = m - 1
                 else:
                     l = m + 1
-            else:
-                if nums[m]<= target <= nums[r]:
-                    l = m+1
+            elif nums[m]<nums[r]:
+                if  nums[m]<target<=nums[r]:
+                    l = m + 1
                 else:
                     r = m - 1
-        return -1
+        return -1 
                 
-        
-        
