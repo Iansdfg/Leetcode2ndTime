2ee4688b46dbb955a158d61079e7f4dc9a6f6388
Tian Feng <feng.txy@outlook.com>
2019-07-30 15:05:17 -0700

    Update 91. Decode Ways.py

--- 91. Decode Ways.py
+++ 91. Decode Ways.py
@@ -4,13 +4,13 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        dp = [0]*(len(s)+1)
-        dp[0] = 1
+        dp = [1]+[0]*(len(s))
         for i in range(1,len(dp)):
-            if s[i-1] != '0':
+            if s[i-1]!='0': 
                 dp[i]+=dp[i-1]
-            if i>1 and '10'<=s[i-2:i]<='26':
+            if i>1 and '10'<= s[i-2:i]<='26':
                 dp[i]+=dp[i-2]
         return dp[-1]
+                
             
         
